73ee93741767ff8e8a556567924af9730e9de650
Diana Rahilda <dianarahilda227@gmail.com>
2020-04-27 13:14:39 +0700

    Perbaikan Tampilan modul Prodi

--- resources/views/admin/prodis/create.blade.php
+++ resources/views/admin/prodis/create.blade.php
@@ -25,13 +25,12 @@
                         @csrf
                         <div class="form-group">
                             <label class="required" for="nama">{{ trans('cruds.prodi.fields.nama') }}</label>
-                            <input class="form-control {{ $errors->has('nama') ? 'is-invalid' : '' }}" type="text" name="nama" id="nama" value="{{ old('nama', '') }}" required>
+                            <input class="form-control {{ $errors->has('nama') ? 'is-invalid' : '' }}" type="text" name="nama" id="nama" value="{{ old('nama', '') }}" placeholder="{{ trans('cruds.prodi.fields.nama_helper') }}" required>
                             @if($errors->has('nama'))
                                 <div class="invalid-feedback">
                                     {{ $errors->first('nama') }}
                                 </div>
                             @endif
-                            <span class="help-block">{{ trans('cruds.prodi.fields.nama_helper') }}</span>
                         </div>
                         <div class="form-group">
                             <label for="fakultas_id">{{ trans('cruds.prodi.fields.fakultas') }}</label>
@@ -45,44 +44,43 @@
                                     {{ $errors->first('fakultas_id') }}
                                 </div>
                             @endif
-                            <span class="help-block">{{ trans('cruds.prodi.fields.fakultas_helper') }}</span>
                         </div>
                         <div class="form-group">
                             <label for="kode">{{ trans('cruds.prodi.fields.kode') }}</label>
-                            <input class="form-control {{ $errors->has('kode') ? 'is-invalid' : '' }}" type="text" name="kode" id="kode" value="{{ old('kode', '') }}">
+                            <input class="form-control {{ $errors->has('kode') ? 'is-invalid' : '' }}" type="text" name="kode" id="kode" value="{{ old('kode', '') }}" placeholder="{{ trans('cruds.prodi.fields.kode_helper') }}">
                             @if($errors->has('kode'))
                                 <div class="invalid-feedback">
                                     {{ $errors->first('kode') }}
                                 </div>
                             @endif
-                            <span class="help-block">{{ trans('cruds.prodi.fields.kode_helper') }}</span>
                         </div>
                         <div class="form-group">
                             <label for="kode_dikti">{{ trans('cruds.prodi.fields.kode_dikti') }}</label>
-                            <input class="form-control {{ $errors->has('kode_dikti') ? 'is-invalid' : '' }}" type="text" name="kode_dikti" id="kode_dikti" value="{{ old('kode_dikti', '') }}">
+                            <input class="form-control {{ $errors->has('kode_dikti') ? 'is-invalid' : '' }}" type="text" name="kode_dikti" id="kode_dikti" value="{{ old('kode_dikti', '') }}" placeholder="{{ trans('cruds.prodi.fields.kode_dikti_helper') }}">
                             @if($errors->has('kode_dikti'))
                                 <div class="invalid-feedback">
                                     {{ $errors->first('kode_dikti') }}
                                 </div>
                             @endif
-                            <span class="help-block">{{ trans('cruds.prodi.fields.kode_dikti_helper') }}</span>
                         </div>
                         <div class="form-group">
                             <label for="akreditasi">{{ trans('cruds.prodi.fields.akreditasi') }}</label>
-                            <input class="form-control {{ $errors->has('akreditasi') ? 'is-invalid' : '' }}" type="text" name="akreditasi" id="akreditasi" value="{{ old('akreditasi', '') }}">
+                            <input class="form-control {{ $errors->has('akreditasi') ? 'is-invalid' : '' }}" type="text" name="akreditasi" id="akreditasi" value="{{ old('akreditasi', '') }}" placeholder="{{ trans('cruds.prodi.fields.akreditasi_helper') }}">
                             @if($errors->has('akreditasi'))
                                 <div class="invalid-feedback">
                                     {{ $errors->first('akreditasi') }}
                                 </div>
                             @endif
-                            <span class="help-block">{{ trans('cruds.prodi.fields.akreditasi_helper') }}</span>
-                        </div>
-                        <div class="form-group">
-                            <button class="btn btn-danger" type="submit">
-                                {{ trans('global.save') }}
-                            </button>
                         </div>
                     </form>
+                </div>
+
+                <div class="card-footer">
+                    <div class="form-group">
+                        <button class="btn btn-danger" type="submit">
+                            {{ trans('global.save') }}
+                        </button>
+                    </div>
                 </div>
             </div>
         </div>
